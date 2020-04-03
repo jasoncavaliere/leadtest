8c716b9b44d8155e74f39803947a02fec377a652
Jason Cavaliere <jason@quarterspot.com>
2020-04-03 10:57:40 -0500

    updated project and program to get the last bits into VCS

--- src/Program.cs
+++ src/Program.cs
@@ -20,7 +20,7 @@ namespace AspNetMaker2020
 				.ConfigureWebHostDefaults(webBuilder =>
 					{
 						webBuilder
-							.UseUrls("http://localhost:5000")
+						//	.UseUrls("http://localhost:5000")
 							.UseStartup<Startup>();
 					});
 	}
