9f01b0e402b2d10eaa804b3a5af34c6ac141433c
Jason Cavaliere <jason@quarterspot.com>
2020-04-02 20:48:15 -0500

    initial commit

---
+++ src/admin.ui/admin.ui/Models/Leads/LeadsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using admin.services;
+
+namespace admin.ui.Models.Leads
+{
+    public class LeadViewModel
+    {
+        public Lead[] Leads { get; set; }
+
+    }
+}
